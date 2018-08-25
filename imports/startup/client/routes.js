2731fccce2433291616ac7d6853ca76ad5757bc9
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-08-25 02:11:30 -0300

    chat files

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,11 +1,21 @@
 import '../../ui/layouts/app';
 import '../../ui/pages/create';
+import '../../ui/pages/chat';
 
 FlowRouter.route('/', {
   name: 'index',
   action () {
     BlazeLayout.render('App', {
       main: 'Create',
+    });
+  },
+});
+
+FlowRouter.route('/chat', {
+  name: 'chat',
+  action () {
+    BlazeLayout.render('App', {
+      main: 'Chat',
     });
   },
 });
