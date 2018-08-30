0915c2ea008700c00ae35f2d68f594c499e1a329
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-08-30 10:52:33 -0300

    clear db on startup

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,3 +1,13 @@
 import '/imports/api/users/server';
 import '/imports/api/meetings/server';
 import '/imports/api/messages/server';
+
+import Users from '/imports/api/meetings';
+import Meetings from '/imports/api/meetings';
+import Messages from '/imports/api/meetings';
+
+Meteor.startup(() => {
+  Users.remove({});
+  Meetings.remove({});
+  Messages.remove({});
+});
